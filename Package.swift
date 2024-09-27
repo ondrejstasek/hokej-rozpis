fdc1f2ce7d8af1be1129935ce2d13ddd41699bf6
ondrejstasek <61665409+ondrejstasek@users.noreply.github.com>
2024-09-28 00:36:34 +0200

    rename

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kalendář s rozpisem hokeje",
+    name: "HockeyScheduleCalendar",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/dehesa/CodableCSV.git", from: "0.6.7"),
@@ -14,7 +14,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Kalendář s rozpisem hokeje",
+            name: "HockeyScheduleCalendar",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "CodableCSV", package: "CodableCSV"),
